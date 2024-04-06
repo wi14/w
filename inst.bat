c811f4c5ea099526ed5a9edf27a71052e86799a4
wi14 <166194924+wi14@users.noreply.github.com>
2024-04-06 16:15:24 +0900

    Update inst.bat

--- inst.bat
+++ inst.bat
@@ -1,8 +1,2 @@
-@echo off
-cd "%appdata%/Microsoft/Windows/Start Menu/Programs/Startup"
-(
-    echo cls
-    echo start rickroll.bat
-    echo ascii.live/can-you-hear-me
-) >rickroll.bat
-rickroll.bat
+echo Hello, World!
+pause
